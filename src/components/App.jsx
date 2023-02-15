342f4b7b86a10794b55b4d879a3afa934e3a0ae6
Viktoriia Slobodeniuk <vikavas988@gmail.com>
2023-02-16 01:34:06 +0200

    4 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import user from '../hw_data/user.json';
 import data from '../hw_data/data.json';
 import friends from '../hw_data/friends.json';
+import transactions from '../hw_data/transactions.json';
 
 import Profile from './Profile';
 import Statistics from './Statistics';
 import FriendList from './FriendList';
+import TransactionHistory from './TransactionHistory';
 
 export const App = () => {
   return (
@@ -16,10 +18,9 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
-
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
